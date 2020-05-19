b48b51c4dc16e847ea60b0c54e500b9a1b33305f
Kristóf <krstf456@gmail.com>
2020-05-19 13:37:08 +0200

    user model

---
+++ Server/Models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    frist_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    isAdmin: {
+        type: booelan,
+        required: true,
+        default: false
+    }
+}, { versionKey: false })
+
+module.exports = User = mongoose.model('users', UserSchema)
