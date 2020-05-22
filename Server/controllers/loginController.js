6826b8d8d202b0922737a583e1649e7c2d251ef2
Kristóf <krstf456@gmail.com>
2020-05-22 15:32:23 +0200

    logincontroller updated

--- Server/controllers/loginController.js
+++ Server/controllers/loginController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
 
 
 module.exports = function register() {
@@ -9,7 +10,22 @@ module.exports = function register() {
     })
       .then(user => {
         if(!user) {
-            
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                userData.password = hash
+                User.create(userData)
+                    .then(user => {
+                        res.status(200).send({ status: user.email + ' Registered'})
+                    })
+                    .catch(err => {
+                        res.send('error: ' + err)
+                    })
+            })
+                
+        } else {
+            res.status(401).send('User already exists')
         }
+      })
+      .catch(err => {
+          res.send('error: ' + err)
       })
 }
