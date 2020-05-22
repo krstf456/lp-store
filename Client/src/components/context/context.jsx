b6c873eb3c84bfa49c6efc957f62cdeb30fbb7ee
Lisa <lisa.bengtsson@live.com>
2020-05-22 12:01:09 +0200

    modal

--- Client/src/components/context/context.jsx
+++ Client/src/components/context/context.jsx
@@ -1,1 +1,56 @@
 import React from 'react';
+
+const Context = React.createContext();
+
+export class Provider extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            showModal: false
+        }
+    }
+
+    //To open modal call this function on a button
+    toggleModal = () => {
+        this.setState({
+          showModal: !this.state.showModal,
+        });
+    };
+
+    //Place modal-content in here
+    get modal() {
+        if (this.state.showModal) {
+          return (
+            <Modal>
+              <div>
+                  <h1>MODAL WITH PRODUCTINFO</h1>
+              </div>
+            </Modal>
+          );
+        }
+        return undefined;
+      }
+
+
+
+
+
+
+
+
+
+      render() {
+        return (
+          <Context.Provider value={this.state}>
+            {this.props.children}
+            {this.modal}
+          </Context.Provider>
+        );
+      }
+
+
+}
+
+export default Context;
+
+export const Consumer = Context.Consumer;
