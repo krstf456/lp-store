0ea927e09f1e0e787eb799408773b55b7c275261
Louise Bäckström <backstromlouise@gmail.com>
2020-05-25 11:20:29 +0200

    added endpoint for login (POST)

--- Server/routes/loginRoute.js
+++ Server/routes/loginRoute.js
@@ -1,1 +1,9 @@
-// Order route
+const express = require('express')
+const router = express.Router()
+let userModel = require('../Models/user.model')
+
+
+// Login user
+router.post('/login', async (req, res,) => {})
+
+module.exports = router
--- Server/server.js
+++ Server/server.js
@@ -15,14 +15,14 @@ app.use(
         extended: false
     })
 )
-//const loginRouter = require('./routes/loginRoute')
+const loginRouter = require('./routes/loginRoute')
 //const orderRouter = require('./routes/orderRoute')
 const productRouter = require('./routes/productRoute')
-//const userRouter = require('./routes/userRoute')
+const userRouter = require('./routes/userRoute')
 
-//app.use('/login', loginRouter)
+app.use('/users', loginRouter)
 //app.use('/order', orderRouter)
 app.use('/products', productRouter)
-//app.use('/user', userRouter)
+app.use('/users', userRouter)
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
