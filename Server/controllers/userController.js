a1099f4ce12c80dd967bc67d21f36eb9a1957742
Kristóf <krstf456@gmail.com>
2020-05-25 10:20:20 +0200

    createNewUser

---
+++ Server/controllers/userController.js
@@ -0,0 +1,31 @@
+const User = require('../models/user.model')
+const bcrypt = require('bcrypt')
+
+
+createNewUser = async (req, res) => {
+        try {
+            const userData = new userModel(req.body)
+            const findUser = await productModel.findOne ({ email: req.body.email })
+
+            if(!findUser) {
+                await bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    userData.password = hash
+                    findUser.create(userData)
+                            .then(user => {
+                                res.status(200).send({ status: user.email + ' Registered'})
+                            })
+                            .catch(err => {
+                                res.send('error: ' + err)
+                            })
+                })
+            } else {
+                res.status(401).send('User is already registered!')
+            } 
+        } catch (err){
+            res.send('error: ' + err)
+
+        }
+    }   
+       
+   
+module.exports = { createNewUser }
