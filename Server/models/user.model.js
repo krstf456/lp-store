088ed3a23ce81ab7beea7a319229ea92493cc0f7
Kristóf <krstf456@gmail.com>
2020-05-25 10:39:39 +0200

    testing register

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -19,7 +19,7 @@ const UserSchema = new Schema({
         type: 'ObjectId',
         ref: 'Adress' 
         
-    },
+     },
     isAdmin: {
         type: booelan,
         required: true,
--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ require('./connect')
 
 const express = require('express')
 const cors = require('cors')
-//const mongoose = require('mongoose')
+
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -18,11 +18,11 @@ app.use(
 //const loginRouter = require('./routes/loginRoute')
 //const orderRouter = require('./routes/orderRoute')
 //const productRouter = require('./routes/productRoute')
-//const userRouter = require('./routes/userRoute')
+const userRouter = require('./routes/userRoute')
 
 //app.use('/login', loginRouter)
 //app.use('/order', orderRouter)
 //app.use('/product', productRouter)
-//app.use('/user', userRouter)
+app.use('/user', userRouter)
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`))
