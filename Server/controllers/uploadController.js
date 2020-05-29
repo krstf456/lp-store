a65e0dc6a54de480dedd3db9a4811dbe9050255d
Oskar Lindblad <lindbladoskar@gmail.com>
2020-05-29 09:43:30 +0200

    Uploading images base code nomongoDB

--- Server/controllers/uploadController.js
+++ Server/controllers/uploadController.js
@@ -1,8 +1,12 @@
-const fileUpload = require('express-fileupload')
+const  uuid = require('uuid')
 
 uploadImage = async (req, res) => {
   console.log(req.files)
-  res.json("success")
+
+  const id = uuid.v4()
+  const imageName = id + req.files.image.name
+  req.files.image.mv('./uploadFile/' + imageName)
+  res.json({src: '/uploadFile/' + imageName})
 };
 
 module.exports = {
--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,13 @@ const { Error404Handler, errorHandler} = require('./handlers/handleErrors')
 
 const express = require('express')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use('/uploadFile', express.static('uploadFile'))
+app.use(fileUpload({createParentPath:true}))
 app.use(cors())
 app.use(express.json())
 
@@ -15,6 +18,7 @@ app.use(
         extended: false
     })
 )
+
 const loginRouter = require('./routes/loginRoute')
 const orderRouter = require('./routes/orderRoute')
 const productRouter = require('./routes/productRoute')
