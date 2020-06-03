f38c0a30bcd84e74cf0c0d01986a144a81f6ee31
Louise Bäckström <backstromlouise@gmail.com>
2020-06-03 14:06:07 +0200

    added onClick for every genre in mainPage

--- Client/src/components/mainPage/mainPage.jsx
+++ Client/src/components/mainPage/mainPage.jsx
@@ -3,16 +3,115 @@ import Context from "../context/context";
 import { Box, ResponsiveContext, Menu } from "grommet";
 
 class MainPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+     showAllAlbums: false,
+     showAllRock: false,
+     showAllSoul: false,
+     showAllPop: false,
+     showAllPsycadelic: false,
+     showAllProg: false,
+     showAllOther: false
+    }
+  }
   //This will enable the use of context-functions and states
   static contextType = Context;
 
   componentDidMount = () => {
+    this.context.getAllAlbums();
     this.context.getAllRock();
     this.context.getAllSoul();
     this.context.getAllPop();
+    //this.context.getAllPsycadelic();
+    //this.context.getAllProg();
+    //this.context.getAllOther();
   };
 
- 
+  handleOnClickAll = () => {
+    this.setState({
+                  showAllAlbums: this.context.displayAllAlbums(),
+                  showAllRock: false,
+                  showAllSoul: false,
+                  showAllPop: false,
+                  showAllPsycadelic: false,
+                  showAllProg: false,
+                  showAllOther: false 
+                  })
+  }
+
+  handleOnClickRock = () => {
+    this.setState({
+                  showAllRock: this.context.displayAllRock(),
+                  showAllAlbums: false,
+                  showAllSoul: false,
+                  showAllPop: false,
+                  showAllPsycadelic: false,
+                  showAllProg: false,
+                  showAllOther: false 
+                  })
+  }
+
+  handleOnClickSoul = () => {
+    this.setState({
+                  showAllSoul: this.context.displayAllSoul(),
+                  showAllRock: false,
+                  showAllAlbums: false,
+                  showAllPop: false,
+                  showAllPsycadelic: false,
+                  showAllProg: false,
+                  showAllOther: false 
+                  })
+  }
+
+  handleOnClickPop = () => {
+    this.setState({
+                  showAllPop: this.context.displayAllPop(),
+                  showAllRock: false,
+                  showAllSoul: false,
+                  showAllAlbums: false,
+                  showAllPop: false,
+                  showAllPsycadelic: false,
+                  showAllProg: false,
+                  showAllOther: false 
+                  })
+  }
+
+  handleOnClickPsycadelic = () => {
+    this.setState({
+                  showAllPsycadelic: this.context.displayAllPop(),
+                  showAllRock: false,
+                  showAllSoul: false,
+                  showAllAlbums: false,
+                  showAllProg: false,
+                  showAllOther: false,
+                  showAllPop: false, 
+                  })
+  }
+
+  handleOnClickProg = () => {
+    this.setState({
+                  showAllProg: this.context.displayAllPop(),
+                  showAllRock: false,
+                  showAllSoul: false,
+                  showAllAlbums: false,
+                  showAllPop: false, 
+                  showAllPsycadelic: false,
+                  showAllOther: false 
+                  })
+  }
+
+  handleOnClickOther = () => {
+    this.setState({
+                  showAllOther: this.context.displayAllPop(),
+                  showAllRock: false,
+                  showAllSoul: false,
+                  showAllAlbums: false,
+                  showAllPsycadelic: false,
+                  showAllProg: false, 
+                  showAllPop: false,
+                  })
+  }
 
   render() {
     return (
