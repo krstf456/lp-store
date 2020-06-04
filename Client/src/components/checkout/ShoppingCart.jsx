cb99da28045fee8668b60563362a7fa1e665abfe
Lisa <lisa.bengtsson@live.com>
2020-06-04 14:12:51 +0200

    cart in progress

--- Client/src/components/checkout/ShoppingCart.jsx
+++ Client/src/components/checkout/ShoppingCart.jsx
@@ -7,92 +7,90 @@ import {
   TableRow,
   TableCell,
   TableBody,
-  ResponsiveContext
+  ResponsiveContext,
 } from "grommet";
 import { Cart, FormAdd, FormSubtract, FormTrash } from "grommet-icons";
-
-
+import Context from "../context/context";
 
 export default class ShoppingCart extends React.Component {
+  static contextType = Context;
+
   render() {
+    localStorage.getItem("cart", this.context.shoppingCart);
+    console.log("render", this.context.shoppingCart);
     return (
-          <ResponsiveContext.Consumer>
-            {size => (
-              <Box pad="large" gap="large" width="large" background="light-1">
-                <Box direction="row" width="large" justify="between">
-                  <Text size="large" weight="bold">
-                    Cart
-                  </Text>
-                  <Cart color="brand"></Cart>
-                </Box>
-                <Box /* style={cartboxContainer(size)} */>
-                  <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableCell scope="col" border="bottom">
-                          <strong>Items</strong>
-                        </TableCell>
-                        <TableCell size="xsmall" scope="col" border="bottom">
-                          <strong></strong>
-                        </TableCell>
-                        <TableCell scope="col" border="bottom">
-                          <strong>Qty</strong>
-                        </TableCell>
-                        <TableCell scope="col" border="bottom">
-                          <strong>Price</strong>
-                        </TableCell>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                          <TableRow>
-                            <TableCell scope="row">
-                              <img
-                              />
-                            </TableCell>
-                            <TableCell size="small">
-                              title
-                            </TableCell>
-                            <TableCell>Quantity</TableCell>
-                            <TableCell>
-                              price * quantity SEK
-                            </TableCell>
-                            <TableCell>
-                              <FormAdd
-                              ></FormAdd>
-                              <FormSubtract
-                              ></FormSubtract>
-                              <FormTrash
-                              ></FormTrash>
-                            </TableCell>
-                          </TableRow>
-                        );
-                      })}
-                      <TableRow>
-                        <TableCell border="bottom"></TableCell>
-                        <TableCell border="bottom"></TableCell>
-                        <TableCell border="bottom"></TableCell>
-                        <TableCell border="bottom"></TableCell>
-                      </TableRow>
-                      <TableRow>
+      <ResponsiveContext.Consumer>
+        {(size) => (
+          <Box pad="large" gap="large" width="large" background="light-1">
+            <Box direction="row" width="large" justify="between">
+              <Text size="large" weight="bold">
+                Cart
+              </Text>
+              <Cart color="brand"></Cart>
+            </Box>
+            <Box /* style={cartboxContainer(size)} */>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableCell scope="col">
+                      <strong>Items</strong>
+                    </TableCell>
+                    <TableCell size="xsmall" scope="col">
+                      <strong>Album</strong>
+                    </TableCell>
+                    <TableCell scope="col">
+                      <strong>Qty</strong>
+                    </TableCell>
+                    <TableCell scope="col">
+                      <strong>Price</strong>
+                    </TableCell>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {this.context.shoppingCart.map((product, index) => {
+                    return (
+                      <TableRow key={index}>
                         <TableCell scope="row">
-                          <strong>Total</strong>
+                          <img style={{height: "3rem", width: "3rem"}}
+                          src={product.image} />
                         </TableCell>
-                        <TableCell scope="row">
-                          <strong></strong>
-                        </TableCell>
+                    <TableCell size="small">{product.album}</TableCell>
+                        <TableCell>Quantity</TableCell>
+                        <TableCell>{product.price} {/* * quantity */} SEK</TableCell>
                         <TableCell>
-                          <strong>total quantity</strong>
-                        </TableCell>
-                        <TableCell>
-                          <strong>totalprice SEK</strong>
+                          <FormAdd></FormAdd>
+                          <FormSubtract></FormSubtract>
+                          <FormTrash></FormTrash>
                         </TableCell>
                       </TableRow>
-                    </TableBody>
-                  </Table>
-                </Box>
-              </Box>
-            )}
-          </ResponsiveContext.Consumer>
+                    );
+                  })}
+                 {/*  <TableRow>
+                    <TableCell border="bottom"></TableCell>
+                    <TableCell border="bottom"></TableCell>
+                    <TableCell border="bottom"></TableCell>
+                    <TableCell border="bottom"></TableCell>
+                  </TableRow> */}
+                  <TableRow>
+                    <TableCell scope="row">
+                      <strong>Total</strong>
+                    </TableCell>
+                    <TableCell scope="row">
+                      <strong></strong>
+                    </TableCell>
+                    <TableCell>
+                      <strong>total</strong>
+                    </TableCell>
+                    <TableCell>
+                      <strong>total</strong>
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </Box>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
     );
   }
 }
