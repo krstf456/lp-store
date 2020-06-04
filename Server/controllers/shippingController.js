c7f1ab19e3b3dcc7073a5fbb000cf085fcc4881d
Lisa <lisa.bengtsson@live.com>
2020-06-04 17:17:07 +0200

    in progress

--- Server/controllers/shippingController.js
+++ Server/controllers/shippingController.js
@@ -13,5 +13,22 @@ getShippingAlternatives = async (req, res, next) => {
     }
   };
 
+  createShippingMethod = async (req, res, next) => {
+    try {
+        const shipping = new shippingModel(req.body);
+        const findShipping = await shippingModel.findOne({ company: req.body.company });
+    
+        // If album doesn't exist, create and save a new one
+        if (!findShipping) {
+          await shipping.save();
+          res.send(shipping);
+        } else {
+          res.status(404).json({message : "Method already exists"})
+        }
+      } catch (err) {
+        next(err)
+      }
+  }
 
-  module.exports = { getShippingAlternatives };
+
+  module.exports = { getShippingAlternatives, createShippingMethod };
--- Server/routes/shippingRoute.js
+++ Server/routes/shippingRoute.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const shippingModel = require('../models/Shipping.model')
 const router = express.Router()
-const { getShippingAlternatives } = require('../controllers/shippingController')
+const { getShippingAlternatives, createShippingMethod } = require('../controllers/shippingController')
 
 
 // Get shipping alternatives
 router.get('/shipping', getShippingAlternatives, async (req, res) => {})
+
+router.post('/shipping', createShippingMethod, async (req, res) => {})
 
 
 module.exports = router
