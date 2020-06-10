12e11fd572a8ffa0d87443967b11a881c964cec6
Lisa <lisa.bengtsson@live.com>
2020-06-10 14:56:21 +0200

    styling

--- Client/src/components/admin/admin.jsx
+++ Client/src/components/admin/admin.jsx
@@ -1,45 +1,44 @@
 import React from "react";
 import Context from "../context/context";
-import userContext from '../context/userContext'
-import { Box, Button, ResponsiveContext } from "grommet";
-import "./Admin.css"
+import userContext from "../context/userContext";
+import { Box, Button, ResponsiveContext, Main } from "grommet";
+import "./Admin.css";
 import { Link } from "react-router-dom";
+import "./Admin.css";
 
 class Admin extends React.Component {
-    //This will enable the use of context-functions and states
-    // static contextType = Context
-    static contextType = userContext
+  //This will enable the use of context-functions and states
+  // static contextType = Context
+  static contextType = userContext;
 
-   
-  
-    render() {
-      return ( 
-        <>
+  render() {
+    return (
+      <>
         {this.context.renderRedirect()}
         <ResponsiveContext.Consumer>
           {(size) => (
+            <div className="adminContainer">
             <Box>
-            
               <h1>Admin</h1>
               <Link to="/admin/orders">
-                <Button label="Orders"/>
+                <Button label="Orders" />
               </Link>
               <Link to="/admin/products">
-                <Button label="Products"/>
+                <Button label="Products" />
               </Link>
               <Link to="/admin/uploadproduct">
-                <Button label="Add Product"/>
+                <Button label="Add Product" />
               </Link>
               <Link to="/admin/users">
-                <Button label="Users"/>
+                <Button label="Users" />
               </Link>
             </Box>
+            </div>
           )}
         </ResponsiveContext.Consumer>
-        </>
-      );
-    }
+      </>
+    );
   }
-  
-  export default Admin;
-  
+}
+
+export default Admin;
