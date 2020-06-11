9835f305777375669b4bdf71eb91ec3f21ce74e6
Viktor Ödman <viktoralexanderodman@gmail.com>
2020-06-11 09:26:52 +0200

    Updated animation

--- Client/src/components/loginPrompt/LoadingAnimation.jsx
+++ Client/src/components/loginPrompt/LoadingAnimation.jsx
@@ -1,33 +1,27 @@
 // UncontrolledLottie.jsx
 import React, { Component } from 'react'
 import Lottie from 'react-lottie'
-import animationData from './data.json'
+import animationData from './loading2.json'
 
 class LoadingAnimation extends Component {
+	render() {
+		const defaultOptions = {
+			loop: true,
+			autoplay: true,
+			animationData: animationData,
+			rendererSettings: {
+				preserveAspectRatio: 'xMidYMid slice',
+			},
+		}
 
-
-  render(){
-
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
-    };
-
-    return(
-      <div>
-        <h1>Lottie</h1>
-        <p>Base animation free from external manipulation</p>
-        <Lottie options={defaultOptions}
-              height={200}
-              width={200}
-        />
-      </div>
-    )
-  }
+		return (
+			<div>
+				<h1>Lottie</h1>
+				<p>Base animation free from external manipulation</p>
+				<Lottie options={defaultOptions} height={200} width={200} />
+			</div>
+		)
+	}
 }
 
 export default LoadingAnimation
