95201ba6e4c4745a8bc26195918791535f2d3e0b
Lisa <lisa.bengtsson@live.com>
2020-06-11 10:47:09 +0200

    added email in invoice section

--- Client/src/components/payment/Invoice.jsx
+++ Client/src/components/payment/Invoice.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { FormField } from "grommet";
+import UserContext from "../context/userContext";
 
 
 export default class Invoice extends React.Component {
+    //This will enable the use of context-functions and states
+    static contextType = UserContext;
+  
   render() {
     return (
           <FormField
+            value={this.context.email}
             name="email"
             label="Email"
             type="text"
