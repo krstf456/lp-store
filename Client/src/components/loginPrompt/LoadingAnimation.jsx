5fd7f82bf9712d668d5d9121561944a8c9e04515
Viktor Ödman <viktoralexanderodman@gmail.com>
2020-06-11 11:05:17 +0200

    updated Animation

--- Client/src/components/loginPrompt/LoadingAnimation.jsx
+++ Client/src/components/loginPrompt/LoadingAnimation.jsx
@@ -1,7 +1,7 @@
 // UncontrolledLottie.jsx
 import React, { Component } from 'react'
 import Lottie from 'react-lottie'
-import animationData from './loading2.json'
+import animationData from './vwCircle.json'
 
 class LoadingAnimation extends Component {
 	render() {
